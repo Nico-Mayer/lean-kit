43a0330a430018d6e1f62aab32811fa1f07e4534
Nico-Mayer <nico.m94@icloud.com>
2023-01-18 13:30:04 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css'
+	import 'virtual:windi.css'
+</script>
+
+<main class="flex h-screen bg-nord6 w-screen">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import 'iconify-icon'
+	let shadow = false
+
+	function toggleShadow() {
+		shadow = !shadow
+	}
+</script>
+
+<section class="flex flex-col m-auto space-y-10 items-center">
+	<div class="bg-white rounded flex h-50 w-50" class:shadow-xl={shadow}>
+		<h1 class="font-bold m-auto text-nord3">lean-kit</h1>
+	</div>
+	<button
+		class="rounded flex bg-nord4 p-2 text-red-500 gap-2 items-center"
+		on:click={toggleShadow}>
+		<iconify-icon
+			icon={!shadow ? 'tabler:shadow-off' : 'tabler:shadow'}
+			class:animate-wobble={shadow}
+			class:animate-swing={!shadow}
+			class="animate-fast text-2xl text-nord0" />
+	</button>
+</section>
