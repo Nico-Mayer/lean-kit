90cacf452a89a6e012d0123ebc17cbb8fb6104f2
Nico-Mayer <nico.m94@icloud.com>
2023-11-30 11:33:11 +0100

    Remove unused files and update dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,5 @@
 <script lang="ts">
 	import '../app.css'
-	import { activeTheme } from '$lib/stores/store'
-	import { onMount } from 'svelte'
-
-	onMount(() => {
-		const localTheme = localStorage.getItem('theme')
-
-		switch (localTheme) {
-			case 'dark':
-				$activeTheme = 'dark'
-				break
-			case 'light':
-				$activeTheme = 'light'
-				break
-			default:
-				$activeTheme = 'dark'
-				break
-		}
-	})
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,4 @@
-<script lang="ts">
-	import { activeTheme } from '$lib/stores/store'
-
-	function toggleTheme() {
-		if ($activeTheme === 'dark') {
-			localStorage.setItem('theme', 'light')
-			document.documentElement.classList.remove('dark')
-			$activeTheme = 'light'
-			return
-		} else {
-			localStorage.setItem('theme', 'dark')
-			document.documentElement.classList.add('dark')
-			$activeTheme = 'dark'
-			return
-		}
-	}
-</script>
-
-<main
-	class="w-screen h-screen dark:bg-black dark:text-white flex items-center justify-center">
-	<button
-		class="flex items-center justify-center gap-4 border p-4 py-2 rounded-md"
-		on:click={toggleTheme}>
-		<div class="i-tabler-sun" />
-		<span>lean-kit</span>
-	</button>
+<main>
+	<div class="i-mdi-home"></div>
+	<h1 class="animate-bounce">Welcome to Lean-Kit</h1>
 </main>
